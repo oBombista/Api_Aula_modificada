9614f91a7b429c4a951f17e6590c25a40ee31783
Tiago Castro <59033674+oBombista@users.noreply.github.com>
2022-11-25 12:47:02 -0300

    0

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,6 @@ app.use(statusRouter);
 app.use(errorHandler);
 app.use(authorizationRoute);
 
-app.listen(4000, () => {
+app.listen("apiaulamodificada-production.up.railway.app", () => {
   console.log('Estou Em FUNCIONAMENTO');
 });
