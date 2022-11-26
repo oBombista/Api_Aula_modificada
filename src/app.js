b6c5460d14b328b314046c5d3d9bb989a212a7f0
Tiago Castro <59033674+oBombista@users.noreply.github.com>
2022-11-25 23:09:10 -0300

    01

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ class App {
         this.middlewares();
         this.routes();
     }
-
+    
     middlewares(){
         this.server.use(express.json());
         this.server.use(cors());
--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,14 @@ import usersRoute from './routes/users.routes';
 import statusRouter from './routes/status.routes';
 import errorHandler from './middllewares/error-handler.middlleware';
 import authorizationRoute from './routes/authorization.routes';
-
+import cors from "cors"
 const app = express();
+
+app.use((req, res, next) =>{
+    res.header("Access-Control-Allow-Origin", "*");
+    app.use(cors());
+    next();
+});
 
 app.use(express.json());
 app.use(usersRoute);
